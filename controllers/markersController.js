0df3f164257c76564e7c084b404dac7ae0075af5
AybarsTurel <aybarsturel15@hotmail.com>
2025-01-12 22:12:40 +0300

    Empty List for tags return 200 with empty list.

--- controllers/markersController.js
+++ controllers/markersController.js
@@ -40,11 +40,7 @@ const addMarker = async (req, res) => {
 const getVerifiedMarkers = async (req, res) => {
     try {
         const verifiedMarkers = await Marker.find({ verified: 1 });
-
-        if (verifiedMarkers.length === 0) {
-            return res.status(404).json({ message: 'No verified markers found' });
-        }
-
+        // Return 200 with an empty array if no markers are found
         res.status(200).json(verifiedMarkers);
     } catch (error) {
         console.error('Error fetching verified markers:', error);
@@ -52,37 +48,32 @@ const getVerifiedMarkers = async (req, res) => {
     }
 };
 
+
 // Get all verified markers controller
 const getRefutedMarkers = async (req, res) => {
     try {
-        const verifiedMarkers = await Marker.find({ verified: 0 });
-
-        if (verifiedMarkers.length === 0) {
-            return res.status(404).json({ message: 'No refuted markers found' });
-        }
-
-        res.status(200).json(verifiedMarkers);
+        const refutedMarkers = await Marker.find({ verified: 0 });
+        // Return 200 with an empty array if no markers are found
+        res.status(200).json(refutedMarkers);
     } catch (error) {
         console.error('Error fetching refuted markers:', error);
         res.status(500).json({ message: error.message });
     }
 };
 
+
 // Get all markers controller
 const getAllMarkers = async (req, res) => {
     try {
         const markers = await Marker.find({});
-
-        if (markers.length === 0) {
-            return res.status(404).json({ message: 'No markers found' });
-        }
-
+        // Return 200 with an empty array if no markers are found
         res.status(200).json(markers);
     } catch (error) {
         console.error('Error fetching markers:', error);
         res.status(500).json({ message: error.message });
     }
 };
+
 
 // Delete marker controller
 const deleteMarker = async (req, res) => {
