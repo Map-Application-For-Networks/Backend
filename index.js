215644f515410f8c5aa7da9515bf43a56e0945d5
Şevki Aybars Türel <105316570+AybarsTurel@users.noreply.github.com>
2025-01-12 23:31:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = 3001;
 
 // Enable CORS for all requests (can restrict it to specific origins if needed)
 app.use(cors(
-    
+    origin : ["https://frontend-indol-kappa-59.vercel.app/"]
 ));
 
 // To restrict CORS to specific origin (like your React app):
